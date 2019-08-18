876bbd0034c2e5cd3bf8b7b4d966a1dc136a8fdd
Simon Walker <s.r.walker101@googlemail.com>
2019-08-18 20:58:09 +0100

    Merge helper files

--- side_extractor.py
+++ side_extractor.py
@@ -6,14 +6,52 @@ import matplotlib.pyplot as plt
 from functools import partial
 import cv2
 from sklearn.cluster import KMeans
-from utils import (
-    get_line_through_points,
-    distance_point_line_squared,
-    distance_point_line_signed,
-    rotate,
-)
 
 _corner_indexes = [(0, 1), (1, 3), (3, 2), (0, 2)]
+
+
+def get_line_through_points(p0, p1):
+    """
+    Given two points p0 (x0, y0) and p1 (x1, y1),
+    compute the coefficients (a, b, c) of the line
+    that passes through both points.
+    """
+    x0, y0 = p0
+    x1, y1 = p1
+
+    return y1 - y0, x0 - x1, x1 * y0 - x0 * y1
+
+
+def distance_point_line_squared(pos1, origin):
+    """
+    Computes the squared distance of a 2D point (x0, y0) from a line ax + by + c = 0
+    """
+    a, b, c = pos1
+    x0, y0 = origin
+    return (a * x0 + b * y0 + c) ** 2 / (a ** 2 + b ** 2)
+
+
+def distance_point_line_signed(pos1, origin):
+    """
+    Computes the signed distance of a 2D point (x0, y0) from a line ax + by + c = 0
+    """
+    a, b, c = pos1
+    x0, y0 = origin
+    return (a * x0 + b * y0 + c) / np.sqrt(a ** 2 + b ** 2)
+
+
+def rotate(image, degrees):
+    """
+    Rotate an image by the amount specifiedi in degrees
+    """
+    if len(image.shape) == 3:
+        rows, cols, _ = image.shape
+    else:
+        rows, cols = image.shape
+
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), degrees, 1)
+
+    return cv2.warpAffine(image, M, (cols, rows)), M
 
 
 def compute_barycentre(thresh, value=0):
