b293ec9a10ebe24b9413d6c90ad43cfa382d2bcd
Simon Walker <s.r.walker101@googlemail.com>
2019-08-18 20:24:37 +0100

    Start main script

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from side_extractor import process_piece
+import cv2
+
+
+if __name__ == "__main__":
+    image_filename = "images/IMG_20180415_211104234.jpg"
+    image = cv2.imread(image_filename)
+
+    result = process_piece(image)
+    import IPython; IPython.embed(); exit()
